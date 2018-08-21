317d4ff6e903542126ec83a7dd453e440434194a
Reinaldo Garcia Maturell <reinaldo.maturell@gmail.com>
2018-08-21 17:30:17 -0400

    Add base files

---
+++ eventchamp-child/functions.php
@@ -0,0 +1,7 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'eventchamp_enqueue_styles' );
+function eventchamp_enqueue_styles() {
+    wp_enqueue_style( 'eventchamp-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'eventchamp-child-style', get_stylesheet_directory_uri() . '/style.css' );
+
+}
